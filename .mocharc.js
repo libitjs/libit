fe11f1b4dc7347c72c7f238fc92276e3febea024
ty <towyuan@outlook.com>
2021-06-17 18:40:42 +0800

    chore: set mocha default timeout to 10s

--- .mocharc.js
+++ .mocharc.js
@@ -3,6 +3,7 @@ const defaultConfig = require('@loopback/build/config/.mocharc.json');
 
 const MONOREPO_CONFIG = {
   parallel: true,
+  timeout: '10000',
 };
 
 module.exports = mergeMochaConfigs(defaultConfig, MONOREPO_CONFIG);
